643d27617d05df1c66e924a420f26adc22fa3c85
Aman <singh1381aman@gmail.com>
2020-06-20 15:28:03 -0700

    Finish signUP

---
+++ src/firebase/test.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('gg4EfkxeC0uefOCBvxQc').collection('cartItems').doc('EQiJHyKLGE4IdSur0ShP')
